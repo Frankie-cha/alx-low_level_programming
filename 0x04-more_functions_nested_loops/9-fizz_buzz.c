43efcecf9f83212d16c1bff9cb6b9384c7f432ed
Frankie-cha <oyiengofrankline49@gmail.com>
2023-10-13 07:59:28 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ for (a = 1; a <= 100; a++)
 {
 if (a % 3 == 0 && a % 5 != 0)
 {
-printf("Fizz");
+printf(" Fizz");
 }
 else if (a % 5 == 0 && a % 3 != 0)
 {
-printf("Buzz");
+printf(" Buzz");
 }
 else if (a % 3 == 0 && a % 5 == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
 else if (a == 1)
 {
