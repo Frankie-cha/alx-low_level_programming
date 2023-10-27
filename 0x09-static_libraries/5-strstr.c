38b27126be66584f31953a003c3398dce82d9aea
Frankie-cha <oyiengofrankline49@gmail.com>
2023-10-27 06:04:30 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr -  function that locates a substring
+ * @haystack: first occurrence of substring
+ * @needle: the string
+ * Return: pointer of the substring if not NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int index;
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+index = 0;
+if (haystack[index] == needle[index])
+{
+do {
+if (needle[index + 1] == '\0')
+return (haystack);
+index++;
+} while (haystack[index] == needle[index]);
+}
+haystack++;
+}
+return ('\0');
+}
